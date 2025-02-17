c448913cba5e0aaf15d83643932adf41c5c7ae7f
Soumyadip Roy <soumyaroy172003@gmail.com>
2025-02-18 00:39:01 +0530

    added render deployed link

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      "/socket.io": {
+        target: "https://gpt-web-container.vercel.app", // Use your backend URL
+        ws: true, // Make sure this is enabled for WebSockets
+      },
+    },
+  },
+});
