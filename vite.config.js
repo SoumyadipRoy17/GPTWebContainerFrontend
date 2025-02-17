ebca12b77a201a4a170b46ff5733865bbd2a38f7
Soumyadip Roy <soumyaroy172003@gmail.com>
2025-02-18 00:42:04 +0530

    ....

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/socket.io": {
-        target: "https://gpt-web-container.vercel.app", // Use your backend URL
-        ws: true, // Make sure this is enabled for WebSockets
-      },
-    },
-  },
 });
+
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     proxy: {
+//       "/socket.io": {
+//         target: "https://gpt-web-container.vercel.app", // Use your backend URL
+//         ws: true, // Make sure this is enabled for WebSockets
+//       },
+//     },
+//   },
+// });
