54a9b10404acd0bed058ef907416347f356e766c
Soumyadip Roy <soumyaroy172003@gmail.com>
2025-05-12 11:52:57 +0530

    updated socket

--- src/config/socket.js
+++ src/config/socket.js
@@ -23,6 +23,7 @@ export const receiveMessage = (eventName, cb) => {
 export const sendMessage = (eventName, data) => {
   console.log("here in socket.js send message");
   socketInstance.emit(eventName, data);
+  console.log("here in socket.js send message end", data);
 };
 
 // import socket from "socket.io-client";
